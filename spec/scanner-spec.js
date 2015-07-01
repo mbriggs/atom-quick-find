62d9e08879846ebbd4770ebb0ff689ed87a7a3fd
Matt Briggs <matt@mattbriggs.net>
2015-07-01 19:49:32 -0400

    text editor scanner

--- spec/scanner-spec.js
+++ spec/scanner-spec.js
@@ -5,7 +5,7 @@ import {TextEditor} from 'atom';
 
 let fixture = `
 a quick brown fox jumps over the lazy dog;
-a quick brown fox jumps over the lazy dog;
+ a Quick brown fox jumps over the working dog;
 `;
 
 describe("Scanner", () => {
@@ -18,9 +18,46 @@ describe("Scanner", () => {
     editor.setText(fixture);
   });
 
+  describe("scanning", () => {
+    it("finds correct number of results", () => {
+      scanner.setExpression('quick');
+      let results = scanner.scan();
+
+      expect(results.length).toBe(2);
+    });
+
+    it("finds correct range", () => {
+      scanner.setExpression('quick');
+      let [first, second] = scanner.scan();
+
+      expect(first.start.column).toBe(2);
+      expect(first.start.row).toBe(1);
+      expect(first.end.column).toBe(7);
+      expect(first.end.row).toBe(1);
+
+      expect(second.start.column).toBe(3);
+      expect(second.start.row).toBe(2);
+      expect(second.end.column).toBe(8);
+      expect(second.end.row).toBe(2);
+    });
+
+    it("does case sensitive searches", () => {
+      scanner.setExpression('Quick', {caseSensitive: true});
+      let results = scanner.scan();
+
+      expect(results.length).toBe(1);
+    });
+
+    it("does regexp searches", () => {
+      scanner.setExpression('the (lazy|working) dog', {regex: true});
+      let results = scanner.scan();
+
+      expect(results.length).toBe(2);
+    });
+  });
+
 
   describe("building expressions", () => {
-
     it("builds a regexp", () => {
       scanner.setExpression('foo');
 
