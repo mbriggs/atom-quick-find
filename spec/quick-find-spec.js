dccbe362bd227f0f9368bd19cc4b51fe40764fa2
Matt Briggs <matt@mattbriggs.net>
2015-07-01 16:25:45 -0400

    dismissable

--- spec/quick-find-spec.js
+++ spec/quick-find-spec.js
@@ -1,16 +1,54 @@
 "use babel";
 
 describe("Quick Find", () => {
-  let workspace;
+  let workspace, activation;
 
-  beforeEach(done => {
+  beforeEach(() => {
     workspace = atom.views.getView(atom.workspace);
-    atom.packages.activatePackage('quick-find').then(done, done);
+    activation = atom.packages.activatePackage('quick-find');
+    jasmine.attachToDOM(workspace);
   });
 
   it('adds a panel', () => {
     atom.commands.dispatch(workspace, 'quick-find:find');
 
-    expect(workspace.querySelector('.quick-find-panel')).toExist();
+    waitsForPromise(() => activation);
+
+    runs(() => {
+      expect(workspace.querySelector('.quick-find-panel')).toExist();
+    });
+  });
+
+  it('only allows one panel', () => {
+    atom.commands.dispatch(workspace, 'quick-find:find');
+    atom.commands.dispatch(workspace, 'quick-find:find');
+
+    waitsForPromise(() => activation);
+
+    runs(() => {
+      expect(workspace.querySelectorAll('.quick-find-panel').length).toBe(1);
+    });
+  });
+
+  it('has a visible panel', () => {
+    atom.commands.dispatch(workspace, 'quick-find:find');
+
+    waitsForPromise(() => activation);
+
+    runs(() => {
+      expect(workspace.querySelector('.quick-find-panel')).toBeVisible();
+    });
+  });
+
+  it('hides the panel when dismissed', () => {
+    atom.commands.dispatch(workspace, 'quick-find:find');
+
+    waitsForPromise(() => activation);
+
+    runs(() => {
+      let panel = workspace.querySelector('.quick-find-panel');
+      atom.commands.dispatch(panel, 'quick-find:dismiss');
+      expect(panel).not.toBeVisible();
+    });
   });
 });
